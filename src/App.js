9ba6bc9502941121d603439bda9745d484b9da6c
NellySP <nelly.petren@gmail.com>
2022-04-25 16:55:16 +0200

    working get random activity!!!!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
 import RandomActivity from "./components/RandomActivity";
-import Button from "./components/Button";
 
 function App() {
-  const test = async () => {
-    console.log("hej!");
-  };
-
   return (
     <div className="app">
       <RandomActivity />
--- src/components/RandomActivity/index.js
+++ src/components/RandomActivity/index.js
@@ -3,21 +3,22 @@ import React, { useState, useEffect } from "react";
 
 const RandomActivity = () => {
   const [activity, setActivity] = useState();
-  useEffect(() => {
-    getActivity();
-  }, []);
 
   const getActivity = async () => {
-    const response = await fetch("http://www.boredapi.com/api/activity");
+    const response = await fetch("https://www.boredapi.com/api/activity");
     const data = await response.json();
     console.log(data);
     setActivity(data.activity);
   };
 
+  useEffect(() => {
+    getActivity();
+  }, []);
+
   return (
     <div>
       <h1>{activity}</h1>
-      <Button />
+      <Button handleClick={() => getActivity()} />
     </div>
   );
 };
